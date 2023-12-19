bb87d3944e51cfe8bc8331cd34aeafd9ad76666c
Bruno Rodríguez <63863884+bruno-rodriguez-evs@users.noreply.github.com>
2023-12-19 21:35:56 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,27 +1,27 @@
-//create a web server that can listen to requests for /hello and /goodbye
-//and respond with some text
-//start the server using node.js
-//check that the server works using curl or a web browser
-//deploy the server to the cloud using now
-//check that the server works on the cloud
-//submit the URL of the server to earn points for this assignment
+// //create a web server that can listen to requests for /hello and /goodbye
+// //and respond with some text
+// //start the server using node.js
+// //check that the server works using curl or a web browser
+// //deploy the server to the cloud using now
+// //check that the server works on the cloud
+// //submit the URL of the server to earn points for this assignment
 
-//import the express module
-const express = require('express')
+// //import the express module
+// const express = require('express')
 
-//create a new web server
-const app = express()
+// //create a new web server
+// const app = express()
 
-//configure the route that the server will respond to
-app.get('/', function (request, response) {
-    response.send('hello world')
-})
+// //configure the route that the server will respond to
+// app.get('/', function (request, response) {
+//     response.send('hello world')
+// })
 
-app.get('/goodbye', function (request, response) {
-    response.send('goodbye world')
-})
+// app.get('/goodbye', function (request, response) {
+//     response.send('goodbye world')
+// })
 
-//start the server
-app.listen(3000, function () {
-    console.log('Server is running on port 3000')
-})
+// //start the server
+// app.listen(3000, function () {
+//     console.log('Server is running on port 3000')
+// })
